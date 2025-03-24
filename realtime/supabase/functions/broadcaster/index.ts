d4807a3bf1c1774387ffb301f911b99664e15f71
nimapdevyash <yash.sawarkar@nimapinfotech.com>
2025-03-24 18:51:52 +0530

    broadcaster is sending messages

---
+++ realtime/supabase/functions/broadcaster/index.ts
@@ -0,0 +1,59 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import {
+  createClient,
+  RealtimeChannel,
+} from "https://esm.sh/@supabase/supabase-js@2.49.1";
+
+function sendMessage(channel: RealtimeChannel, message: string): void {
+  channel.send({
+    type: "broadcast",
+    event: "message",
+    payload: { message },
+  });
+}
+
+Deno.serve(async (req) => {
+  try {
+    const client = createClient(
+      Deno.env.get("SUPABASE_URL")!,
+      Deno.env.get("SUPABASE_ANON_KEY")!,
+    );
+
+    const group_chat_channel = client.channel("group_chat");
+
+    const { message } = await req.json();
+
+    sendMessage(group_chat_channel, message);
+
+    return new Response(
+      JSON.stringify({
+        status: "success",
+      }),
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        status: "failed",
+        error,
+      }),
+    );
+  }
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/broadcaster' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
