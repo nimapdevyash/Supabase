0416a5b2a4df8b026dda801fb5a27dadc91d5ef7
nimapdevyash <yash.sawarkar@nimapinfotech.com>
2025-03-24 19:31:05 +0530

    functions work properly only when we don't send response , needs more inspection of the issue , but app sends and reads the events

--- realtime/supabase/functions/person_one/index.ts
+++ realtime/supabase/functions/person_one/index.ts
@@ -3,45 +3,20 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
-import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import {
-  createClient,
-  RealtimeChannel,
-} from "https://esm.sh/@supabase/supabase-js@2.49.1";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.49.1";
 
-function sendMessage(channel: RealtimeChannel, message: string): void {
-  channel.send({
-    type: "broadcast",
-    event: "message",
-    payload: { message },
-  });
-}
-
-Deno.serve(async (req): any => {
+Deno.serve(async (req) => {
   try {
     const client = createClient(
       Deno.env.get("SUPABASE_URL")!,
       Deno.env.get("SUPABASE_ANON_KEY")!,
     );
 
-    const group_chat_channel = client.channel("group_chat", {
-      config: {
-        broadcast: {
-          self: false,
-          ack: false,
-        },
-      },
-    });
+    const group_chat_channel = client.channel("group_chat");
 
-    await group_chat_channel.subscribe((status) => {
-      if (status !== "SUBSCRIBED") {
-        console.error("Failed to subscribe to the channel");
-      }
-    });
-
-    const { message } = await req.json();
-
-    sendMessage(group_chat_channel, message);
+    group_chat_channel.on("broadcast", { event: "*" }, (message) => {
+      console.log("message recieved : ", message);
+    }).subscribe();
   } catch (error) {
     console.log(error);
     return new Response(
@@ -58,7 +33,7 @@ Deno.serve(async (req): any => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/broadcaster' \
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/person_one' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
